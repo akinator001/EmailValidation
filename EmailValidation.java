5e9b42e1c4abd494b0f8597e95abfdf68a1677e7
akinator001 <aakashgandhi1279@gmail.com>
2020-09-29 21:15:52 +0530

    [aakash] Added uc1(start mandatory part) , created method

--- EmailValidation.java
+++ EmailValidation.java
@@ -1,9 +1,30 @@
 package com.cp.emailvalidation;
 
-public class EmailValidation {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
 
+public class EmailValidation {
+	
+	static Scanner sc = new Scanner(System.in);
+	
+	public static void emailValidator() {
+		System.out.println("Enter your Email :");
+		String email = sc.next();
+		
+		Pattern pattern = Pattern.compile("^abc.*$");
+		Matcher matcher = pattern.matcher(email);
+		Boolean bool = matcher.find();
+		if (bool)
+			System.out.println("Email is Valid");
+		else
+			System.out.println("Email is Unvalid");
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Email validation");
+		EmailValidation email = new EmailValidation();
+	    EmailValidation.emailValidator();
 	}
 
 }
