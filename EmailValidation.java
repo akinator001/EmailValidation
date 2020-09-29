85eec2b55ca052103c3262a7785e94bcd599d897
akinator001 <aakashgandhi1279@gmail.com>
2020-09-29 21:29:18 +0530

    [aakash] Added uc2(2nd mandatory part), modified regex

--- EmailValidation.java
+++ EmailValidation.java
@@ -12,7 +12,7 @@ public class EmailValidation {
 		System.out.println("Enter your Email :");
 		String email = sc.next();
 		
-		Pattern pattern = Pattern.compile("^abc.*$");
+		Pattern pattern = Pattern.compile("^abc[.a-zA-Z0-9]*(@bridgelabz).*$");
 		Matcher matcher = pattern.matcher(email);
 		Boolean bool = matcher.find();
 		if (bool)
